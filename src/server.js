d00c7392f68e2077b7f0a7bf9b5c5a6581647cd9
Nguyen Ngoc Chinh <chinhnguyen.210399@gmail.com>
2022-04-23 09:17:51 +0700

    Add engine template handlebars

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.engine("hbs", engine({ defaultLayout: "home/master", extname: ".hbs" }));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.render("home/index");
+});
+
+app.listen(port, () => console.log(`App listen at port: ${port}`));
